63685c234865e3a7c4b203384c0897f97c40dc75
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-03-04 15:50:19 +0800

    Create ban.py

---
+++ .slash/ban.py
@@ -0,0 +1,8 @@
+import servers
+import membersfetch from servers 
+
+async def on_message():
+  if message.content.startswith('/ban' + user + until + reason):
+    then:
+      membersfetch.ban('{user} with {until} with {reason}')
+  
