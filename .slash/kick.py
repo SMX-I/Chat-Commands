196d30104981f17ebd057b0d7056bc8d177010c5
DarkBYT153#4092 <91132683+ShadowPlazs@users.noreply.github.com>
2022-03-04 15:47:52 +0800

    Create kick.py

---
+++ .slash/kick.py
@@ -0,0 +1,7 @@
+import servers
+import membersfetch from servers
+
+async def on_message():
+  if message.content.startswith('/kick' + user + reason):
+    then:
+      membersfetch.kick('{user} with {reason}')
